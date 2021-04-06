f5d1cd3e905000ab5aa61837521e11727b383abf
lizzyphannn <81193607+lizzyphannn@users.noreply.github.com>
2021-04-06 18:27:52 -0400

    Add files via upload

---
+++ Mission_03.py
@@ -0,0 +1,6 @@
+num1 = int(input("Pick a number: "))
+num2 = int(input("Pick another number: "))
+num3 = int(input("Pick another number: "))
+num4 = int(input("Pick another number: "))
+result = (num1 * num2 * num3 * num4)
+print("The product of " + str(num1) + " * " + str(num2) + " * " + str(num3) + " * " + str(num4) + " is " + str(result))
